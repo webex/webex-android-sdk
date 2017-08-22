6215a60fc962f5fd10901ebfead7a1882921cc57
Allen Shaw <xionxiao@cisco.com>
2017-08-22 15:20:52 +0800

    [SSDK-1131] disable commonlib autologin

--- sdk/src/main/java/com/ciscospark/core/SparkApplicationDelegate.java
+++ sdk/src/main/java/com/ciscospark/core/SparkApplicationDelegate.java
@@ -1,17 +1,13 @@
 package com.ciscospark.core;
 
 
-import com.cisco.spark.android.app.AndroidSystemServicesModule;
-import com.cisco.spark.android.core.AndroidModule;
 import com.cisco.spark.android.core.Application;
 import com.cisco.spark.android.core.ApplicationDelegate;
-import com.cisco.spark.android.core.BaseSquaredModule;
-import com.cisco.spark.android.core.LiveProximityModule;
 import com.cisco.spark.android.core.RootModule;
-import com.cisco.spark.android.core.SquaredModule;
 import com.github.benoitdion.ln.DebugLn;
 import com.github.benoitdion.ln.Ln;
 import com.github.benoitdion.ln.NaturalLog;
+import com.squareup.leakcanary.RefWatcher;
 
 import javax.inject.Inject;
 
@@ -30,19 +26,19 @@ class SparkApplicationDelegate extends ApplicationDelegate {
         applicationModule = new SparkApplicationModule();
     }
 
+    @Override
     protected void onCreate() {
-        this.initializeObjectGraph();
-        this.objectGraphCreated();
-        this.inject();
-        this.afterInject();
+
     }
 
+    @Override
     protected void objectGraphCreated() {
         RootModule.setInjector(this);
+        applicationModule.setRefWatcher(RefWatcher.DISABLED);
     }
 
+    @Override
     protected void afterInject() {
-        Ln.i(bus.toString());
 
     }
 
@@ -51,38 +47,14 @@ class SparkApplicationDelegate extends ApplicationDelegate {
         return new DebugLn();
     }
 
+    @Override
     protected Object getApplicationModule() {
         return applicationModule;
     }
 
-    private void inject() {
-        this.objectGraph.inject(this);
-
-        this.bus.register(this);
-    }
-
-    private void initializeObjectGraph() {
-        if (this.bus != null) {
-            this.bus.unregister(this);
-        }
-
-        if (this.objectGraph == null) {
-            this.objectGraph = ObjectGraph.create(this.getModules());
-        }
-
-    }
-
-    protected Object[] getModules() {
-        return new Object[]{new RootModule(this), new AndroidModule(), new SquaredModule(), new BaseSquaredModule(), new AndroidSystemServicesModule(), new LiveProximityModule(), this.getApplicationModule()};
-    }
-
     @Override
-    public void inject(Object o) {
-
-    }
-
-    @Override
-    public ObjectGraph getObjectGraph() {
-        return null;
+    public void create(boolean startAuthenticatedUserTask) {
+        /* Override this function to prevent common lib auto login */
+        super.create(false);
     }
 }
