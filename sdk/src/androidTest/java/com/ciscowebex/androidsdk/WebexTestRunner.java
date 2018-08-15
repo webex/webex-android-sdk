908e58d77c6a4b755a7f55c9c0788c0147867d63
Allen Shaw <xionxiao@cisco.com>
2018-08-15 13:38:55 +0800

    add Message client instrument test

--- sdk/src/androidTest/java/com/ciscowebex/androidsdk/WebexTestRunner.java
+++ sdk/src/androidTest/java/com/ciscowebex/androidsdk/WebexTestRunner.java
@@ -25,17 +25,104 @@ package com.ciscowebex.androidsdk;
 
 import android.app.Application;
 import android.content.Context;
+import android.os.Handler;
 import android.support.test.runner.AndroidJUnitRunner;
+
+import com.ciscowebex.androidsdk.auth.OAuthTestUserAuthenticator;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 
 public class WebexTestRunner extends AndroidJUnitRunner {
+    private static String SparkUserEmail;
+    private static String SparkUserName;
+    private static String SparkUserPwd;
+    private static String CLIENT_ID;
+    private static String CLIENT_SEC;
+    private static String REDIRECT_URL;
+    private static String SCOPE;
 
     static Application application;
+    static Webex webex;
+
+    public static Webex getSpark() {
+        return webex;
+    }
 
     @Override
     public Application newApplication(ClassLoader cl, String className, Context context) throws InstantiationException, IllegalAccessException, ClassNotFoundException {
+        System.out.println("!!! newApplication !!!");
         application = super.newApplication(cl, Application.class.getName(), context);
+        new Handler().post(() -> loginBySparkId());
         return application;
     }
 
+    private void loginBySparkId() {
+        System.out.println("!!! loginBySparkId !!!");
+        final CountDownLatch signal = new CountDownLatch(1);
+        File file = new File(application.getExternalFilesDir("login"), "login.txt");
+        if (file.exists()) {
+            HashMap map = readKeyValueTxtToMap(file);
+            SparkUserEmail = (String) map.get("SparkUserEmail");
+            SparkUserName = (String) map.get("SparkUserName");
+            SparkUserPwd = (String) map.get("SparkUserPwd");
+            CLIENT_ID = (String) map.get("CLIENT_ID");
+            CLIENT_SEC = (String) map.get("CLIENT_SEC");
+            REDIRECT_URL = (String) map.get("REDIRECT_URL");
+            SCOPE = (String) map.get("SCOPE");
+            OAuthTestUserAuthenticator auth = new OAuthTestUserAuthenticator(CLIENT_ID, CLIENT_SEC, SCOPE, REDIRECT_URL,
+                    SparkUserEmail, SparkUserName, SparkUserPwd);
+            webex = new Webex(application, auth);
+            auth.authorize(result -> {
+                if (result.isSuccessful()) {
+                    System.out.println("loginBySparkId isSuccessful!");
+                } else {
+                    System.out.println("loginBySparkId failed!");
+                }
+                signal.countDown();
+            });
+        } else {
+            System.out.println("!!! login file is not exist !!!");
+        }
+        try {
+            signal.await(60, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private HashMap readKeyValueTxtToMap(File file) {
+        while (true) {
+            final HashMap keyValueMap = new HashMap();
+            while (true) {
+                try {
+                    final InputStream open = new FileInputStream(file);
+                    final byte[] readArray = new byte[open.available()];
+                    open.read(readArray);
+                    open.close();
+                    final StringTokenizer allLine = new StringTokenizer(new String(readArray, "UTF-8"), "\r\n");
+                    while (allLine.hasMoreTokens()) {
+                        final StringTokenizer oneLine = new StringTokenizer(allLine.nextToken(), "=");
+                        final String leftKey = oneLine.nextToken();
+                        if (!oneLine.hasMoreTokens()) {
+                            break;
+                        }
+                        final String rightValue = oneLine.nextToken();
+                        keyValueMap.put(leftKey, rightValue);
+                    }
+                    return keyValueMap;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    return keyValueMap;
+                }
+            }
+        }
+    }
 }
---
+++ sdk/src/androidTest/java/com/ciscowebex/androidsdk/message/MessageClientTest.java
@@ -0,0 +1,142 @@
+package com.ciscowebex.androidsdk.message;
+
+import com.ciscowebex.androidsdk.Webex;
+import com.ciscowebex.androidsdk.space.Space;
+import com.ciscowebex.androidsdk.space.SpaceClient;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+import static com.ciscowebex.androidsdk.WebexTestRunner.getSpark;
+import static junit.framework.Assert.assertNotNull;
+import static junit.framework.Assert.assertTrue;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class MessageClientTest {
+
+    private static Webex webex;
+    private static MessageClient messageClient;
+    private static Space room;
+    private static Message msg;
+
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+        webex = getSpark();
+        messageClient = getSpark().messages();
+        createRoom();
+    }
+
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {
+        removeRoom();
+    }
+
+    private static void createRoom() throws InterruptedException {
+        final CountDownLatch signal = new CountDownLatch(1);
+        SpaceClient roomClient = webex.spaces();
+        roomClient.create("message test room", null, result -> {
+            if (result.isSuccessful()) {
+                System.out.println(result.getData());
+                room = result.getData();
+            } else {
+                System.out.println(result.getError());
+            }
+            signal.countDown();
+        });
+        signal.await(30, TimeUnit.SECONDS);
+    }
+
+    private static void removeRoom() throws InterruptedException {
+        System.out.println(":::: remove room");
+        final CountDownLatch signal = new CountDownLatch(1);
+        SpaceClient roomClient = webex.spaces();
+        roomClient.delete(room.getId(), result -> signal.countDown());
+        signal.await(30, TimeUnit.SECONDS);
+    }
+
+    private void waitForMessage(CountDownLatch signal, String text) {
+        messageClient.setMessageObserver(event -> {
+            assertThat(event, instanceOf(MessageObserver.MessageArrived.class));
+            msg = ((MessageObserver.MessageArrived) event).getMessage();
+            assertNotNull(msg);
+            assertThat(msg.getText(), is(equalTo(text)));
+            messageClient.setMessageObserver(null);
+            signal.countDown();
+        });
+    }
+
+    @Test
+    public void testA1_post() throws InterruptedException {
+        System.out.println("::::post 1");
+        assertNotNull(room);
+        final String text = "test post 1";
+        final CountDownLatch signal = new CountDownLatch(1);
+
+        messageClient.post(room.getId(), null, null, text, null, null, result -> {
+            assertTrue(result.isSuccessful());
+        });
+        waitForMessage(signal, text);
+        signal.await(30, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void testA2_post() throws InterruptedException {
+        System.out.println("::::post 2");
+        assertNotNull(room);
+        final String text = "test post 2";
+
+        final CountDownLatch signal = new CountDownLatch(1);
+        messageClient.post(room.getId(), text, null, null, result -> {
+            assertTrue(result.isSuccessful());
+        });
+        waitForMessage(signal, text);
+        signal.await(30, TimeUnit.SECONDS);
+    }
+
+
+    @Test
+    public void testB_list() throws InterruptedException {
+        System.out.println("::::list");
+        final CountDownLatch signal = new CountDownLatch(1);
+        messageClient.list(room.getId(), null, null, null, 10, result -> {
+            assertTrue(result.isSuccessful());
+            signal.countDown();
+        });
+        signal.await(30, TimeUnit.SECONDS);
+    }
+
+
+    @Test
+    public void testC_get() throws InterruptedException {
+        System.out.println("::::get");
+        assertNotNull(msg);
+        final CountDownLatch signal = new CountDownLatch(1);
+        messageClient.get(msg.getId(), result -> {
+            assertTrue(result.isSuccessful());
+            signal.countDown();
+        });
+        signal.await(30, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void testD_delete() throws InterruptedException {
+        System.out.println("::::delete");
+        assertNotNull(msg);
+        final CountDownLatch signal = new CountDownLatch(1);
+        messageClient.delete(msg.getId(), result -> {
+            assertTrue(result.isSuccessful());
+            signal.countDown();
+        });
+        signal.await(30, TimeUnit.SECONDS);
+    }
+}
