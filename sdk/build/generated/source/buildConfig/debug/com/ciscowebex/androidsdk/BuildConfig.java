527aa51ba42bd83264a9c33c0016b375250447e3
QimingDeng <qimdeng@cisco.com>
2018-10-31 16:30:13 +0800

    javaDoc for v2.0

---
+++ sdk/build/generated/source/buildConfig/debug/com/ciscowebex/androidsdk/BuildConfig.java
@@ -0,0 +1,24 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.ciscowebex.androidsdk;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.ciscowebex.androidsdk";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "2.0.0-SNAPSHOT";
+  // Fields from build type: debug
+  public static final String CLIENT_ID = "";
+  public static final String CLIENT_SEC = "";
+  public static final String REDIRECT_URL = "";
+  public static final String SCOPE = "";
+  public static final String TEST_USER_EMAIL = "";
+  public static final String TEST_USER_NAME = "";
+  public static final String TEST_USER_PWD = "";
+  // Fields from default config.
+  public static final String BUILD_REVISION = "159b7e3";
+  public static final String BUILD_TIME = "2018_10_31_16_27_26";
+}
